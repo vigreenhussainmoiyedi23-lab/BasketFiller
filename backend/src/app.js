12243f5379935d6d02f17199130f0a893df5cec9
alvafist <1redhatimmoiyedi13@gmail.com>
2025-12-29 04:30:47 +0530

    deployed successfully

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 // requiring Middlewares
 const { UserCanAcces } = require("./middlewares/AuthenticationMiddleware");
 const adminUrl = "https://basket-filler.vercel.app/";
-const userUrl = "http://localhost:5173";
+const userUrl = "https://basket-filler-ixng.vercel.app/";
 
 // _______requiring all the routers____
 const authRoutes = require("./routes/auth.routes");
